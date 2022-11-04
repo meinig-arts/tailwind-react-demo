d9fd710cda80313d75b515590c6d2ff467f8de69
meinig-arts <michael.meinig@t-online.de>
2022-11-04 19:19:03 +0100

    Finish Recipe Component and use it
    Finish Recipe component; use it to render the pizza dough recipe.
    Needed to copy the pizza image to public/

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import './App.css';
 import Recipe from './Recipe.js';
 function App() {
   return (
-    <Recipe></Recipe>
+    <Recipe name="Pizzateig" image="pizza.jpg" ingredients={['500 ml Wasser','1000g Mehl', '40g Hefe', '20g Salz', '25ml Öl', '1 Prise Zucker']} 
+      description={['Mehl mit Salz auf die Arbeitsfläche oder in eine Schüssel sieben, in die Mitte eine Mulde drücken.',
+       'Hefe mit Wasser und Zucker in die Mulde gießen.',
+       'Von der Mitte her alles gründlich vermischen, nach und nach Olivenöl zugeben und alles zu einem losen Teig verarbeiten.',
+       'Den Teig auf der Arbeitsfläche mit den Händen 5 Minuten kräftig kneten, bis er elastisch und geschmeidig ist.',
+       'Teig zugedeckt bei Zimmertemperatur etwa 30-45 Minuten gehen lassen.']}    />
   );
 }
 
--- src/Recipe.js
+++ src/Recipe.js
@@ -1,7 +1,26 @@
-const Recipe = ({recipe}) => {
-  return <div class="mb-3 font-bold text-2xl">Rezept</div>;
+const Recipe = (recipe) => {
+  return (
+  <div class="container mx-auto my-2 p-2 rounded border">
+    <h1 class="mb-3 font-bold text-2xl">{recipe.name}</h1>
+    <div class="flex md:flex-row flex-col md:space-x-8">
+      <img src={recipe.image} alt="" class="filter blur-md mx-auto md:mx-0" width="300"/>
+      <div>
+        <h2 class="mb-2 font-bold text-l">Zutaten</h2>
+        {recipe.ingredients.map((ingredient, index) => (  
+          <div key={index} class="even:bg-gray-200 even:rounded px-2">{ingredient}</div>
+        ))}
+
+      </div>
+    </div>
+    <div>
+      <h2 class="mt-8 mb-2 font-bold text-l">Zubereitung</h2> 
+      {recipe.description.map((desc) => (
+        <p>{desc}</p>
+      ))}
+    </div>
+  </div>
+  );
 };
 
 export default Recipe;
-
 
